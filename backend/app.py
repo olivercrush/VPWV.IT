3602f392af69212664eeda18f96b2342d75c0755
Olivier B <olivier.beguinti@gmail.com>
2019-10-28 15:15:21 -0400

    Create processImage method

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/processImage', methods=['POST'])
+def processImage():
+    if request.method == 'POST':
+        return 'OK'
+    else
+        return 'NOT OK'
